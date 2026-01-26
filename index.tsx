ab02e4c99f5ea0684ecc1a52055ca1f4b6d03012
DoQuanghealing <doduongquang8686@gmail.com>
2026-01-26 09:37:26 +0700

    Update index.tsx

--- index.tsx
+++ index.tsx
@@ -1,15 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// Đảm bảo tệp App.tsx nằm cùng thư mục và đã có 'export default App'
 import App from './App';
 
+/**
+ * Khởi tạo ứng dụng React
+ * Kết nối với thẻ <div id="root"></div> trong tệp index.html
+ */
 const rootElement = document.getElementById('root');
-if (!rootElement) {
-  throw new Error("Could not find root element to mount to");
-}
 
-const root = ReactDOM.createRoot(rootElement);
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+if (!rootElement) {
+  // Lỗi này thường xảy ra nếu script chạy trước khi DOM kịp tải xong
+  console.error("Không tìm thấy phần tử 'root'. Kiểm tra lại index.html!");
+} else {
+  const root = ReactDOM.createRoot(rootElement);
+  
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+}
